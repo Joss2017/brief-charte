6407bbba34a91864412f077192cd530e7ac571fe
Josselin BAPTISTE <josselin.baptiste@sncf.fr>
2022-10-23 00:14:51 +0200

    MAJ

---
+++ script.js
@@ -0,0 +1,33 @@
+function cache1() {
+  let userInput = document.getElementById("post2");
+  if (userInput.style.display === "none") {
+    userInput.style.display = "block";
+  } else {
+    userInput.style.display = "none";
+  }
+}
+
+function cache2() {
+  let userInput = document.getElementById("post2");
+  if (userInput.style.display === "none") {
+    userInput.style.display = "block";
+  } else {
+    userInput.style.display = "none";
+  }
+}
+function cache3() {
+  let userInput = document.getElementById("post3");
+  if (userInput.style.display === "none") {
+    userInput.style.display = "block";
+  } else {
+    userInput.style.display = "none";
+  }
+}
+function cache4() {
+  let userInput = document.getElementById("post4");
+  if (userInput.style.display === "none") {
+    userInput.style.display = "block";
+  } else {
+    userInput.style.display = "none";
+  }
+}
